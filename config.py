3ed1a664c05749726da494ca1a428f8f42f77569
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-09 22:48:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "16681004")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "161b61f5a06dd299a3d88a3384b9f104")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6646028262').split()]
 
 PORT = os.environ.get("PORT", "8080")
